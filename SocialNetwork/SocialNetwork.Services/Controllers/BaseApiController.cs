f3810f1547d4b365ecc0461350d582073abc3720
Martin Andreev <martin.andreev7@gmail.com>
2015-09-01 15:59:43 +0300

    Register and logout implemented

--- SocialNetwork/SocialNetwork.Services/Controllers/BaseApiController.cs
+++ SocialNetwork/SocialNetwork.Services/Controllers/BaseApiController.cs
@@ -4,6 +4,7 @@
     using Data.Data;
     using Data.Interfaces;
     using Infrastructure;
+    using Microsoft.AspNet.Identity;
 
     public class BaseApiController : ApiController
     {
@@ -21,5 +22,33 @@
         protected IUserIdProvider UserIdProvider { get; set; }
 
         protected ISocialNetworkData Data { get; private set; }
+
+        protected IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return this.InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        this.ModelState.AddModelError(string.Empty, error);
+                    }
+                }
+
+                if (this.ModelState.IsValid)
+                {
+                    return this.BadRequest();
+                }
+
+                return this.BadRequest(this.ModelState);
+            }
+
+            return null;
+        }
     }
 }
