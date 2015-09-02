2d4f158edc2fd557ccf0a53436986a0f823d849f
Mkey5 <marekradkov@gmail.com>
2015-09-02 17:57:00 +0300

    adding deleting model

---
+++ SocialNetwork/SocialNetwork.Services/Models/BindingModels/Post/DeletePostBindingModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Services.Models.BindingModels.Post
+{
+    public class DeletePostBindingModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string AuthorId { get; set; }
+
+        [Required]
+        public string WallOwnerId { get; set; }
+    }
+}
